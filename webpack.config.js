0bb94b4e34e5be79c4040e865c67ec6e0520d699
xiyou <494949543@qq.com>
2022-06-30 21:50:05 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "yqlgq - 一起来搞钱",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
