d51e1e77b6cf705102c9123b86d8c7e1ec3076c7
xiyou <494949543@qq.com>
2022-07-01 23:08:05 +0800

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "yqlgq - 一起来搞钱",
+      template: "src/assets/index.html",
+    })
+  ]
+};
