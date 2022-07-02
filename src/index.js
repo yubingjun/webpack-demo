c4eb1fbf4ba493ebb706e5490483f94b7301f7f8
xiyou <494949543@qq.com>
2022-07-02 18:03:26 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import x from './x.js'
-console.log(x)
-console.log('hi2');
+import png from "./assets/1.png"
+const div = document.getElementById('app')
+console.log(png)
+
+div.innerHTML = `
+    <img src = "${png}">
+`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=> {
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{
+        console.log("模块加载错误")
+    })
+}
+
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,6 +11,35 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "yqlgq - 一起来搞钱",
       template: "src/assets/index.html",
-    })
-  ]
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/i,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
